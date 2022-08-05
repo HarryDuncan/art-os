f1dd34d8133bc429e1d9fdd52befb9b122ca2686
Your Name <you@example.com>
2022-08-05 18:33:11 +1000

    rename to Art-OS

--- src/app/views/landing/Landing.tsx
+++ src/app/views/landing/Landing.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { AnimationWidget } from '../../../visual/visual-components/animation-widget';
-import { Overlay } from '../../components/overlay/Overlay';
-import { LandingContainer, LandingPageTitle } from './Landing.styles';
+import React from "react";
+import { AnimationWidget } from "../../../visual/visual-components/animation-widget";
+import { Overlay } from "../../components/overlay/Overlay";
+import { LandingContainer, LandingPageTitle } from "./Landing.styles";
 
 export function Landing() {
-  const shaderSceneNames = ['liquid', 'cosmic'];
+  const shaderSceneNames = ["liquid", "cosmic"];
 
   const landingScenes = shaderSceneNames.map((scene: string) => ({
-    title: 'shaderScene',
-    name: 'GenericShaderScene',
+    title: "shaderScene",
+    name: "GenericShaderScene",
     data: { shaderName: scene },
   }));
 
   return (
     <LandingContainer>
       <Overlay>
-        <LandingPageTitle>Welcome To GLO</LandingPageTitle>
+        <LandingPageTitle>Art-OS</LandingPageTitle>
       </Overlay>
       <AnimationWidget scenes={landingScenes} />
     </LandingContainer>
